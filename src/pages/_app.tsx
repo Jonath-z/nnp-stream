467f3bfea66521a980c4811bf611b4e371f40bf3
Jonath-z <jonathanzihindula95@gmail.com>
2024-12-28 11:15:05 +0200

    chore: setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { HydrationBoundary, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactElement, ReactNode, useState } from "react";
+import { NextPage } from "next";
+import { appWithTranslation } from "next-i18next";
+import nextI18NextConfig from "../../next-i18next.config";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+/**
+ * Represents a Next.js page with a custom layout.
+ * @typedef {NextPage & { getLayout?: (page: ReactElement) => ReactNode }} NextPageWithLayout
+ */
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+/**
+ * Props for the App component with a custom layout.
+ * @typedef {AppProps & { Component: NextPageWithLayout }} AppPropsWithLayout
+ */
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function App({ Component, pageProps, ...rest }: AppPropsWithLayout) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  const getLayout = Component.getLayout || ((page) => page);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>{getLayout(<Component {...pageProps} />)}</HydrationBoundary>
+    </QueryClientProvider>
+  );
 }
+
+export default appWithTranslation(App, nextI18NextConfig);
